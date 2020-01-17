57c8bda5772f172fb73f600bda3f62036a80d5a9
kt-pbnbs731 <59944761+kt-pbnbs731@users.noreply.github.com>
2020-01-17 09:36:39 +0900

    Update handler.py

--- handler.py
+++ handler.py
@@ -5,26 +5,4 @@ def handle(req):
     Args:
         req (str): request body
     """
-    _server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        print('Server Started')
-        _server.bind(('0.0.0.0', 80))
-        _server.listen(5)
-        print('Wait Client ...')
-        # 接続待受
-        _client, (_client_address, _client_port) = \
-            _server.accept()
-
-        # 受信したメッセージを出力
-        recv_raw_msg = _client.recv(4096)
-        recv_msg = recv_raw_msg.decode()
-        print('Recv={}'.format(recv_msg))
-        _client.send(recv_raw_msg)
-        _client.close()
-    except KeyboardInterrupt:
-        print('Socket Server End')
-    except Exception as err:
-        print('Socket Server Error : {}'.format(err))
-    finally:
-        _server.close()
     return "Python serverless on Unubo Cloud."
